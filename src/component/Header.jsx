aa2447e2bcdb3032bcf18be4dff0d9258244add4
Baabuu-10 <baabuulahar2@gmail.com>
2025-01-26 21:17:36 +0300

    3

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Hero";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Header;
